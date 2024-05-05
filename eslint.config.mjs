daae73bc8d0ba6e6b5e2eec24f9c4787edaabacb
Matheus <matheusmendesreis@outlook.com>
2024-05-05 20:30:49 -0300

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from "@eslint/js";
+import prettier from 'eslint-config-prettier';
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
@@ -7,5 +8,6 @@ export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...prettier.configs.recommended
   
 ];
